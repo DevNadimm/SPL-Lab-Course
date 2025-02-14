4bd5c3d67828e2937d16670410d8c346166d6818
DevNadimm <nadimchowdhury87@gmail.com>
2025-02-14 19:16:00 +0600

    Add initial C programs for basic algorithms and calculations

---
+++ lecture 1/problem-4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char c;
+
+    scanf("%d %c %d", &a, &c, &b);
+    switch (c){
+    case '+' : 
+        printf("%d %c %d = %d", a, c, b, a+b);
+        break;
+    case '-' : 
+        printf("%d %c %d = %d", a, c, b, a-b);
+        break;
+    case '/' : 
+        if (b == 0) {
+            printf("Error: Division by zero is not allowed.");
+        } else {
+            printf("%d %c %d = %d", a, c, b, a/b);
+        }
+        break;
+    case '*' : 
+        printf("%d %c %d = %d", a, c, b, a*b);
+        break;
+    default:
+        printf("Your character is not valid!");
+        break;
+    }
+    return 0;
+}
